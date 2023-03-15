3f49e1eeec7e1d5f70ad238ab861a379828c5cf5
Gustavo Buziki <gustavo.buziki@paipe.co>
2023-03-15 08:41:30 -0300

    first commit

---
+++ src/components/form/input.tsx
@@ -0,0 +1,31 @@
+import {
+    FormControl,
+    FormLabel,
+    Input as InputText,
+    InputProps as PropsInput
+} from '@chakra-ui/react'
+
+interface InputProps extends PropsInput {
+    label?: string
+    name: string,
+}
+
+export const Input = ({label, name, ...props }: InputProps) => {
+    return (
+        <FormControl>
+            {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+                <InputText
+                    name={name}
+                    id={name}
+                    bgColor='pink.300'
+                    focusBorderColor='pink.500'
+                    variant='filled'
+                    _hover={{
+                        bgColor: 'gray.900'
+                    }}
+                    size='lg'
+                    {...props}
+                    />
+            </FormControl>
+    )
+}
